a3813fd85459dbb564cadcbe1522002f7cd104dd
mehmetcnural61 <mehmetnural61@hotmail.com>
2022-06-20 16:48:46 +0300

    Java101 Kosullu İfadeler

---
+++ src/KodBloklariScope/Main.java
@@ -0,0 +1,18 @@
+package KodBloklariScope;
+
+public class Main {
+    //Main  sınıfına ait alan
+
+    int a = 10;
+
+
+    public static void main(String[] args) {
+        // Main metota ait alan
+
+        int a = 5;
+
+        System.out.println(a);
+
+
+    }
+}
