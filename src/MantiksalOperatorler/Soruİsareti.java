197b37fe09c4e29d7a7169aba0665f17aeb919c7
mehmetcnural61 <mehmetnural61@hotmail.com>
2022-06-20 16:36:00 +0300

    Java101 Kosullu İfadeler

---
+++ src/MantiksalOperatorler/Soruİsareti.java
@@ -0,0 +1,10 @@
+package MantiksalOperatorler;
+
+public class Soruİsareti {
+    public static void main(String[] args) {
+       int a=10 , b=20 , c;
+        c=(a==b) ?  2 : 3;
+        System.out.println(c);
+
+    }
+}
