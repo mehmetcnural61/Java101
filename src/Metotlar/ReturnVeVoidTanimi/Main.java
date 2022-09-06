63a105da81be18f6923028635183771f234fb479
mehmetcnural61 <mehmetnural61@hotmail.com>
2022-09-06 21:05:10 +0300

    Return ve Void Tanimi

---
+++ src/Metotlar/ReturnVeVoidTanimi/Main.java
@@ -0,0 +1,14 @@
+package Metotlar.ReturnVeVoidTanimi;
+
+public class Main {
+  static   int sum(int a, int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+
+       int result= sum(5,2) + sum(3,1);
+        System.out.println(result);
+
+
+    }
+}
---
+++ src/Metotlar/ReturnVeVoidTanimi/VoidMain.java
@@ -0,0 +1,13 @@
+package Metotlar.ReturnVeVoidTanimi;
+
+public class VoidMain {
+
+    static void  sum(int a, int b){
+        int result = a+b;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        sum(5,2);
+    }
+}
