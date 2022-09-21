67f4e9e439ac08471887a20b087b2c47f924ad13
mehmetcnural61 <mehmetnural61@hotmail.com>
2022-09-21 19:45:53 +0300

    Pratik-Ögrenci Bilgi Sistemi

---
+++ src/Classes/PratikOgrenciBilgiSistemi/Main.java
@@ -0,0 +1,28 @@
+package Classes.PratikOgrenciBilgiSistemi;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Teacher t1=new Teacher("Mehmet","TRH","90555");
+        Teacher t2=new Teacher("Marcus","FZK","0000");
+        Teacher t3=new Teacher("Brian","BIO","1111");
+
+
+        Course tarih=new Course("Tarih","101","TRH");
+        tarih.addTeacher(t1);
+        Course fizik=new Course("Fizik","102","FZK");
+        fizik.addTeacher(t2);
+        Course biyo=new Course("Biyoloji","103","BIO");
+        biyo.addTeacher(t3);
+
+        Student s1=new Student("Şaban","123","4",tarih,fizik,biyo);
+        s1.addBulkExamNote(100,200,50);
+        s1.isPass();
+
+        Student s2=new Student("Düdük Necmi","456","4",tarih,fizik,biyo);
+        s2.addBulkExamNote(55,60,55);
+        s2.isPass();
+
+    }
+}
---
+++ src/Classes/PratikOgrenciBilgiSistemi/Teacher.java
@@ -0,0 +1,20 @@
+package Classes.PratikOgrenciBilgiSistemi;
+
+public class Teacher {
+
+    String name;
+    String mpno;
+    String branch;
+
+    Teacher(String name,String branch,String mpno){
+        this.name=name;
+        this.branch=branch;
+        this.mpno=mpno;
+    }
+    void print(){
+        System.out.println("Adı: "+this.name);
+        System.out.println("Bölümü: "+this.branch);
+        System.out.println("TelefonNo: "+this.mpno);
+
+    }
+}
