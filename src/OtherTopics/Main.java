a30ce9ba9efc8a4160f451785783a581133d9787
mehmetcnural61 <mehmetnural61@hotmail.com>
2022-10-06 22:34:47 +0300

    Other Topics Math Classes & Methods

---
+++ src/OtherTopics/Main.java
@@ -0,0 +1,4 @@
+package OtherTopics;
+
+public class Main {
+}
---
+++ src/OtherTopics/MathClassAndMethods/Main.java
@@ -0,0 +1,14 @@
+package OtherTopics.MathClassAndMethods;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(Math.abs(-100));
+        System.out.println("---");
+        System.out.println(Math.random()*100);
+        System.out.println("---");
+        System.out.println(Math.round(10.44));
+        System.out.println("---");
+
+    }
+}
