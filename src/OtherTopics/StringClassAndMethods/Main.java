4c85e89b1fda1d1472d37c4411561c9bd4c36005
mehmetcnural61 <mehmetnural61@hotmail.com>
2022-10-11 23:25:18 +0300

    Other Topics String Class & Methods

---
+++ src/OtherTopics/StringClassAndMethods/Main.java
@@ -0,0 +1,13 @@
+package OtherTopics.StringClassAndMethods;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String str="Patika";
+        System.out.println(str.charAt(2));
+
+        char a=97;
+        System.out.println(a);
+
+    }
+}
